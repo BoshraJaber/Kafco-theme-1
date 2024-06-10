24e303ca482aa164c995eb72f3d2e19b8464d7d3
boshrajaber <boshrajaber94@gmail.com>
2024-06-10 08:40:19 +0300

    Add Custom Blocks

--- patterns/footer.php
+++ patterns/footer.php
@@ -7,122 +7,126 @@
  */
 ?>
 
-<!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50"}}},"layout":{"type":"constrained"}} -->
-<div class="wp-block-group" style="padding-top:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50)">
-	<!-- wp:columns {"align":"wide"} -->
-	<div class="wp-block-columns alignwide">
-		<!-- wp:column {"width":"30%"} -->
-		<div class="wp-block-column" style="flex-basis:30%">
-			<!-- wp:group {"style":{"dimensions":{"minHeight":""},"layout":{"selfStretch":"fit","flexSize":null}},"layout":{"type":"flex","orientation":"vertical"}} -->
-			<div class="wp-block-group">
-				<!-- wp:site-logo {"width":20,"shouldSyncIcon":true,"style":{"layout":{"selfStretch":"fit","flexSize":null}}} /-->
+<!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50","left":"var:preset|spacing|50","right":"var:preset|spacing|50"}},"background":{"backgroundImage":{"url":"https://medevword.com/wp-content/uploads/2024/04/footer-bg-3.png","id":223,"source":"file","title":"footer-bg"}},"color":{"background":"#092543"}},"className":"footer-bg","layout":{"type":"flex","justifyContent":"space-between","flexWrap":"nowrap"}} -->
+<div class="wp-block-group alignwide footer-bg has-background" style="background-color:#092543;padding-top:var(--wp--preset--spacing--50);padding-right:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50);padding-left:var(--wp--preset--spacing--50)"><!-- wp:columns {"style":{"layout":{"selfStretch":"fixed","flexSize":"100%"}}} -->
+<div class="wp-block-columns"><!-- wp:column {"width":"46%","metadata":{"name":"Contact Info"},"className":"contact-info"} -->
+<div class="wp-block-column contact-info" style="flex-basis:46%"><!-- wp:paragraph {"className":"genericheading headline headline\u002d\u002dsmall  text\u002d\u002dwhite text\u002d\u002dsmaller footer-heading"} -->
+<p class="genericheading headline headline--small  text--white text--smaller footer-heading">Kuwait Aviation Fuelling Company (KAFCO)</p>
+<!-- /wp:paragraph -->
 
-				<!-- wp:site-title {"level":0,"fontSize":"medium"} /-->
+<!-- wp:group {"metadata":{"name":""},"style":{"dimensions":{"minHeight":"0px"},"layout":{"selfStretch":"fixed","flexSize":"100%"}},"layout":{"type":"flex"}} -->
+<div class="wp-block-group" style="min-height:0px"><!-- wp:group {"className":"align-items-start","layout":{"type":"flex","flexWrap":"nowrap"}} -->
+<div class="wp-block-group align-items-start"><!-- wp:image {"id":192,"sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full"><img src="http://localhost/blocktheme/wp-content/uploads/2024/04/phone-call.png" alt="" class="wp-image-192"/></figure>
+<!-- /wp:image -->
 
-				<!-- wp:site-tagline {"fontSize":"small"} /-->
-			</div>
-			<!-- /wp:group -->
-		</div>
-		<!-- /wp:column -->
+<!-- wp:group {"layout":{"type":"flex","orientation":"vertical"}} -->
+<div class="wp-block-group"><!-- wp:paragraph {"align":"left","className":"genericheading headline headline\u002d\u002dsmall  text\u002d\u002dwhite text\u002d\u002dsmaller text-small"} -->
+<p class="has-text-align-left genericheading headline headline--small  text--white text--smaller text-small">Tel : +(965) 1835000 / 23828000</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
 
-		<!-- wp:column {"width":"20%"} -->
-		<div class="wp-block-column" style="flex-basis:20%">
-		</div>
-		<!-- /wp:column -->
+<!-- wp:group {"className":"align-items-start","layout":{"type":"flex","flexWrap":"nowrap"}} -->
+<div class="wp-block-group align-items-start"><!-- wp:image {"id":410,"sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full"><img src="https://medevword.com/wp-content/uploads/2024/06/location.png" alt="" class="wp-image-410"/></figure>
+<!-- /wp:image -->
 
-		<!-- wp:column {"width":"50%"} -->
-		<div class="wp-block-column" style="flex-basis:50%">
-			<!-- wp:group {"layout":{"type":"flex","flexWrap":"wrap","justifyContent":"space-between","verticalAlignment":"top"}} -->
-			<div class="wp-block-group">
-				<!-- wp:group {"layout":{"type":"flex","orientation":"vertical","justifyContent":"stretch"}} -->
-				<div class="wp-block-group">
-					<!-- wp:heading {"level":2,"style":{"typography":{"fontStyle":"normal","fontWeight":"600"}},"fontFamily":"body"} -->
-					<h2 class="wp-block-heading has-medium-font-size has-body-font-family" style="font-style:normal;font-weight:600"><?php esc_html_e( 'About', 'kafco-custom-theme-1' ); ?></h2>
-					<!-- /wp:heading -->
+<!-- wp:group {"className":"gap-0","layout":{"type":"flex","orientation":"vertical","justifyContent":"left"}} -->
+<div class="wp-block-group gap-0"><!-- wp:paragraph {"className":"genericheading headline headline\u002d\u002dsmall  text\u002d\u002dwhite text\u002d\u002dsmaller text-small"} -->
+<p class="genericheading headline headline--small  text--white text--smaller text-small">P.O.Box: 1654,</p>
+<!-- /wp:paragraph -->
 
-					<!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|10"}},"layout":{"type":"flex","orientation":"vertical"}} -->
-					<div class="wp-block-group">
+<!-- wp:paragraph {"align":"left","className":"genericheading headline headline\u002d\u002dsmall  text\u002d\u002dwhite text\u002d\u002dsmaller text-small"} -->
+<p class="has-text-align-left genericheading headline headline--small  text--white text--smaller text-small">Postal Code: No. 13017, Safat, Kuwait</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
 
-						<!-- wp:navigation {"overlayMenu":"never","layout":{"type":"flex","orientation":"vertical"},"style":{"typography":{"fontStyle":"normal","fontWeight":"400"},"spacing":{"blockGap":"var:preset|spacing|10"}},"fontSize":"small","ariaLabel":"<?php esc_attr_e( 'About', 'kafco-custom-theme-1' ); ?>"} -->
+<!-- wp:group {"className":"align-items-start","layout":{"type":"flex","flexWrap":"nowrap"}} -->
+<div class="wp-block-group align-items-start"><!-- wp:image {"id":413,"sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full"><img src="https://medevword.com/wp-content/uploads/2024/06/email.png" alt="" class="wp-image-413"/></figure>
+<!-- /wp:image -->
 
-						<!-- wp:navigation-link {"label":"<?php esc_html_e( 'Team', 'kafco-custom-theme-1' ); ?>","url":"#"} /-->
-						<!-- wp:navigation-link {"label":"<?php esc_html_e( 'History', 'kafco-custom-theme-1' ); ?>","url":"#"} /-->
-						<!-- wp:navigation-link {"label":"<?php esc_html_e( 'Careers', 'kafco-custom-theme-1' ); ?>","url":"#"} /-->
+<!-- wp:group {"className":"gap-0","layout":{"type":"flex","orientation":"vertical","justifyContent":"left"}} -->
+<div class="wp-block-group gap-0"><!-- wp:paragraph {"className":"genericheading headline headline\u002d\u002dsmall  text\u002d\u002dwhite text\u002d\u002dsmaller text-small"} -->
+<p class="genericheading headline headline--small  text--white text--smaller text-small">General inquiries:</p>
+<!-- /wp:paragraph -->
 
-						<!-- /wp:navigation -->
+<!-- wp:paragraph {"align":"left","className":"genericheading headline headline\u002d\u002dsmall  text\u002d\u002dwhite text\u002d\u002dsmaller text-small"} -->
+<p class="has-text-align-left genericheading headline headline--small  text--white text--smaller text-small">airfuel@kafco.com</p>
+<!-- /wp:paragraph -->
 
-					</div>
-					<!-- /wp:group -->
-				</div>
+<!-- wp:paragraph {"align":"left","className":"genericheading headline headline\u002d\u002dsmall  text\u002d\u002dwhite text\u002d\u002dsmaller text-small"} -->
+<p class="has-text-align-left genericheading headline headline--small  text--white text--smaller text-small">ales &amp; Customer Service inquiries:</p>
+<!-- /wp:paragraph -->
 
-				<!-- /wp:group -->
+<!-- wp:paragraph {"align":"left","className":"genericheading headline headline\u002d\u002dsmall  text\u002d\u002dwhite text\u002d\u002dsmaller text-small"} -->
+<p class="has-text-align-left genericheading headline headline--small  text--white text--smaller text-small">marketing@kafco.com</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column -->
 
-				<!-- wp:group {"layout":{"type":"flex","orientation":"vertical","justifyContent":"stretch"}} -->
-				<div class="wp-block-group">
-					<!-- wp:heading {"level":2,"style":{"typography":{"fontStyle":"normal","fontWeight":"600"}},"fontFamily":"body"} -->
-					<h2 class="wp-block-heading has-medium-font-size has-body-font-family" style="font-style:normal;font-weight:600"><?php esc_html_e( 'Privacy', 'kafco-custom-theme-1' ); ?></h2>
-					<!-- /wp:heading -->
+<!-- wp:column {"width":"40%","metadata":{"name":"Footer Links"},"className":"footer-links"} -->
+<div class="wp-block-column footer-links" style="flex-basis:40%"><!-- wp:group {"metadata":{"name":"Imoportant Links"},"style":{"dimensions":{"minHeight":"0px"},"layout":{"selfStretch":"fixed","flexSize":"100%"}},"className":"important-links","layout":{"type":"flex"}} -->
+<div class="wp-block-group important-links" style="min-height:0px"><!-- wp:paragraph {"className":"genericheading headline headline\u002d\u002dsmall  text\u002d\u002dwhite text\u002d\u002dsmaller footer-heading"} -->
+<p class="genericheading headline headline--small  text--white text--smaller footer-heading">Important Links</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
 
-					<!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|10"}},"layout":{"type":"flex","orientation":"vertical"}} -->
-					<div class="wp-block-group">
+<!-- wp:navigation {"ref":371,"textColor":"base-2","overlayMenu":"never","layout":{"type":"flex","orientation":"vertical"}} /-->
 
-						<!-- wp:navigation {"overlayMenu":"never","layout":{"type":"flex","orientation":"vertical"},"style":{"typography":{"fontStyle":"normal","fontWeight":"400"},"spacing":{"blockGap":"var:preset|spacing|10"}},"fontSize":"small","ariaLabel":"<?php esc_attr_e( 'Privacy', 'kafco-custom-theme-1' ); ?>"} -->
+<!-- wp:group {"metadata":{"name":"Contact Us"},"style":{"dimensions":{"minHeight":"0px"},"layout":{"selfStretch":"fixed","flexSize":"100%"}},"className":"contact-us","layout":{"type":"flex","orientation":"vertical"}} -->
+<div class="wp-block-group contact-us" style="min-height:0px"><!-- wp:paragraph {"className":"genericheading headline headline\u002d\u002dsmall  text\u002d\u002dwhite text\u002d\u002dsmaller footer-heading"} -->
+<p class="genericheading headline headline--small  text--white text--smaller footer-heading">Connect with Us</p>
+<!-- /wp:paragraph -->
 
-						<!-- wp:navigation-link {"label":"<?php esc_html_e( 'Privacy Policy', 'kafco-custom-theme-1' ); ?>","url":"#"} /-->
-						<!-- wp:navigation-link {"label":"<?php esc_html_e( 'Terms and Conditions', 'kafco-custom-theme-1' ); ?>","url":"#"} /-->
-						<!-- wp:navigation-link {"label":"<?php esc_html_e( 'Contact Us', 'kafco-custom-theme-1' ); ?>","url":"#"} /-->
+<!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+<div class="wp-block-group"><!-- wp:image {"id":403,"sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full"><img src="https://medevword.com/wp-content/uploads/2024/06/youtube.png" alt="" class="wp-image-403"/></figure>
+<!-- /wp:image -->
 
-						<!-- /wp:navigation -->
+<!-- wp:image {"id":404,"sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full"><img src="https://medevword.com/wp-content/uploads/2024/06/instagram.png" alt="" class="wp-image-404"/></figure>
+<!-- /wp:image --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column -->
 
-					</div>
-					<!-- /wp:group -->
-				</div>
-				<!-- /wp:group -->
+<!-- wp:column {"width":"40%","metadata":{"name":"Footer Logos"},"className":"footer-logos"} -->
+<div class="wp-block-column footer-logos" style="flex-basis:40%"><!-- wp:paragraph {"className":"genericheading headline headline\u002d\u002dsmall  text\u002d\u002dwhite text\u002d\u002dsmaller  footer-heading"} -->
+<p class="genericheading headline headline--small  text--white text--smaller  footer-heading">International Affiliations &amp; Certificates</p>
+<!-- /wp:paragraph -->
 
-				<!-- wp:group {"layout":{"type":"flex","orientation":"vertical","justifyContent":"stretch"}} -->
-				<div class="wp-block-group">
-					<!-- wp:heading {"level":2,"style":{"typography":{"fontStyle":"normal","fontWeight":"600"}},"fontFamily":"body"} -->
-					<h2 class="wp-block-heading has-medium-font-size has-body-font-family" style="font-style:normal;font-weight:600"><?php esc_html_e( 'Social', 'kafco-custom-theme-1' ); ?></h2>
-					<!-- /wp:heading -->
+<!-- wp:group {"metadata":{"name":""},"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+<div class="wp-block-group"><!-- wp:image {"id":122,"sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full"><img src="https://medevword.com/wp-content/uploads/2024/04/iso-logo.png" alt="" class="wp-image-122"/></figure>
+<!-- /wp:image -->
 
-					<!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|10"}},"layout":{"type":"flex","orientation":"vertical"}} -->
-					<div class="wp-block-group">
+<!-- wp:image {"id":121,"sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full"><img src="https://medevword.com/wp-content/uploads/2024/04/lata-logo.png" alt="" class="wp-image-121"/></figure>
+<!-- /wp:image -->
 
-						<!-- wp:navigation {"overlayMenu":"never","layout":{"type":"flex","orientation":"vertical"},"style":{"typography":{"fontStyle":"normal","fontWeight":"400"},"spacing":{"blockGap":"var:preset|spacing|10"}},"fontSize":"small","ariaLabel":"<?php esc_attr_e( 'Social Media', 'kafco-custom-theme-1' ); ?>"} -->
+<!-- wp:image {"id":120,"sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full"><img src="https://medevword.com/wp-content/uploads/2024/04/jic-logo.png" alt="" class="wp-image-120"/></figure>
+<!-- /wp:image -->
 
-						<!-- wp:navigation-link {"label":"<?php esc_html_e( 'Facebook', 'kafco-custom-theme-1' ); ?>","url":"#"} /-->
-						<!-- wp:navigation-link {"label":"<?php esc_html_e( 'Instagram', 'kafco-custom-theme-1' ); ?>","url":"#"} /-->
-						<!-- wp:navigation-link {"label":"<?php esc_html_e( 'Twitter/X', 'kafco-custom-theme-1' ); ?>","url":"#"} /-->
+<!-- wp:image {"id":167,"sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full"><img src="http://localhost/blocktheme/wp-content/uploads/2024/04/rospa-logo.png" alt="" class="wp-image-167"/></figure>
+<!-- /wp:image --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:group -->
 
-						<!-- /wp:navigation -->
+<!-- wp:group {"style":{"color":{"background":"#161720"}},"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
+<div class="wp-block-group has-background" style="background-color:#161720"><!-- wp:ourblocktheme/genericheading {"text":"Copyright © 2024 Kuwait Aviation Fuelling Company (KAFCO)","size":"small","className":"headline\u002d\u002dsmall text\u002d\u002dwhite text-small"} -->
+<p class="wp-block-ourblocktheme-genericheading headline headline--small  text--white text-small">Copyright © 2024 Kuwait Aviation Fuelling Company (KAFCO)</p>
+<!-- /wp:ourblocktheme/genericheading -->
 
-					</div>
-					<!-- /wp:group -->
-				</div>
-				<!-- /wp:group -->
-			</div>
-			<!-- /wp:group -->
-		</div>
-		<!-- /wp:column -->
-	</div>
-	<!-- /wp:columns -->
-
-	<!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"top":"var:preset|spacing|50","bottom":"0"}}}} -->
-	<div class="wp-block-group alignwide" style="padding-top:var(--wp--preset--spacing--50);padding-bottom:0">
-		<!-- wp:paragraph {"style":{"elements":{"link":{"color":{"text":"var:preset|color|contrast"}}}},"textColor":"contrast-2","fontSize":"small"} -->
-		<p class="has-contrast-2-color has-text-color has-link-color has-small-font-size">
-		<?php
-			/* Translators: WordPress link. */
-			$wordpress_link = '<a href="' . esc_url( __( 'https://wordpress.org', 'kafco-custom-theme-1' ) ) . '" rel="nofollow">WordPress</a>';
-			echo sprintf(
-				/* Translators: Designed with WordPress */
-				esc_html__( 'Designed with %1$s', 'kafco-custom-theme-1' ),
-				$wordpress_link
-			);
-			?>
-		</p>
-		<!-- /wp:paragraph -->
-	</div>
-	<!-- /wp:group -->
-</div>
+<!-- wp:ourblocktheme/genericheading {"text":"Website Terms of Use and Conditions","size":"small","className":"headline\u002d\u002dsmalltext\u002d\u002dwhite text-small"} -->
+<p class="wp-block-ourblocktheme-genericheading headline headline--small  text--white headline--smalltext--white text-small">Website Terms of Use and Conditions</p>
+<!-- /wp:ourblocktheme/genericheading --></div>
 <!-- /wp:group -->
