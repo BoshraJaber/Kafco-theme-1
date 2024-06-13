03cb5dfd4404ecd54f22a1d4615d76c0edbffd79
vijaykumargajam <vjgajam@gmail.com>
2024-06-13 12:13:27 +0000

    permalink_added

--- shortcode/shortcodes.php
+++ shortcode/shortcodes.php
@@ -51,6 +51,7 @@ function latest_news_shortcode(){
 				$news_short_description =  get_field( "news_short_description" );
 				$news_full_description =  get_field( "news_full_description" );
 				$news_gallery = get_field( "news_gallery" );
+				$pagelink=  get_permalink(get_the_ID());  
 				?>
 				<div class="news_box">
 						<div class="news_img">
@@ -61,7 +62,7 @@ function latest_news_shortcode(){
 								<div class="news_title"><?php echo $news_title;?></div>
 								<div class="news_short_description"><?php echo $news_short_description;?></div>
 								<div class="read_more">
-									<a href="">Read More <span>
+									<a href="<?php echo $pagelink;?>">Read More <span>
 										<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right-circle" viewBox="0 0 16 16">
 											<path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8m15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0M4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5z"></path>
 										</svg></span>
